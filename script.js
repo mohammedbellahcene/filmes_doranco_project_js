77a75703cdd93e547a5e14069edfde59e074f0db
bellamoh <mohammedbellahcene95@gmail.com>
2024-10-23 14:47:42 +0200

    ajout de bouttons por rechercher les films par type

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 
-const containerFilms=document.querySelector(".container")
+const containerFilms=document.querySelector(".container");
+const btnPoular=document.querySelector(".popular");
+const btnrated=document.querySelector(".rated");
+const btnupcoming=document.querySelector(".upcoming");
+const titrePrincipal=document.querySelector("h1");
+
 
 const optionsAll = {
     method: "GET",
@@ -10,23 +15,30 @@ const optionsAll = {
     },
   };
 
-  
-async function getData(){
-    const response= await  fetch("https://api.themoviedb.org/3/movie/popular?language=fr-FR&page=1",optionsAll);
+const preposter= "https://media.themoviedb.org/t/p/w220_and_h330_face"
+
+async function getData(type){
+    const response= await  fetch(`https://api.themoviedb.org/3/movie/${type}?language=fr-FR&page=1`,optionsAll);
     const {results}= await response.json();
     console.log(results)
-    containerFilms.innerHTML=results.map (film=>`
+    titrePrincipal.innerHTML=`List of ${type=="top_rated"? "top rated":type } films page 1.`
+    //containerFilms.insertAdjacentHTML("beforebegin",h1)
+    containerFilms.innerHTML=results.map ((film,index)=>`
         <article class="cardfilm">
                 
-                    <img src="https://media.themoviedb.org/t/p/w220_and_h330_face${film.poster_path}" alt="">
+                 <img src="${preposter}${film.poster_path}" alt="">
                 
                 <div class="description">
                     <h2>${film.original_title}</h2>
                     <p>${film.overview}</p>
                 </div>
        
-      </article>`).join("");    
+      </article>`).join(""); 
+      //return containerFilms.innerHTML;   
 }
-getData();
+btnPoular.addEventListener("click",()=>getData("popular"));
+btnrated.addEventListener("click",()=>getData("top_rated"));
+btnupcoming.addEventListener("click",()=>getData("upcoming"));
+//getData("popular");
 
 
